11965d9ffcb23876965decc8df8d2c6ac0fe57d9
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2020-04-13 16:52:05 -0500

    Add routing

--- src/layout/App.js
+++ src/layout/App.js
@@ -1,17 +1,78 @@
 import React, { Component } from 'react';
-import Header from './Header';
-import Sidebar from './Sidebar';
+//import Header from './Header';
+//import Sidebar from './Sidebar';
+import Main from './Main';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+const routes = [
+    {
+        path: "/login",
+        component: Login
+    },
+    {
+        path: "",
+        exact: true,
+        component: Main,
+        routes: [
+            {
+                path: "/home",
+                component: Home
+            }
+        ]
+    }
+];
 
 class App extends Component {
 
-  render() {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    {routes.map((route, i) => (
+                      <RouteWithSubRoutes key={i} {...route} />
+                    ))}
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+function RouteWithSubRoutes(route) {
+  return (
+    <Route
+      path={route.path}
+      render={props => (
+        // pass the sub-routes down to keep nesting
+        <route.component {...props} routes={route.routes} />
+      )}
+    />
+  );
+}
+
+function Login() {
+  return <h2>Login</h2>;
+}
+
+function Home() {
     return (
-      <>
-        <Header />
-        <Sidebar />
-      </>
+        <>
+            <div className="content-header">
+                <div className="container-fluid">
+                    Home
+                </div>
+            </div>
+            <section className="content">
+                <div className="container-fluid">
+                    <h2>Home page</h2>
+                </div>
+            </section>
+        </>
     );
-  }
-};
+}
 
 export default App;
---
+++ src/layout/Main.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch
+} from "react-router-dom";
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            routes: []
+        };
+    }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Sidebar />
+        <div className="content-wrapper">
+            <Switch>
+                {this.props.routes.map((route, i) => (
+                  <RouteWithSubRoutes key={i} {...route} />
+                ))}
+            </Switch>
+        </div>
+      </div>
+    );
+  }
+};
+
+function RouteWithSubRoutes(route) {
+    return (
+      <Route
+        path={route.path}
+        render={props => (
+          // pass the sub-routes down to keep nesting
+          <route.component {...props} routes={route.routes} />
+        )}
+      />
+    );
+}
+
+export default Main;
