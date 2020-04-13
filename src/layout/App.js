69340c788333ebf7d35f31b903bd2774e8a93a53
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2020-04-13 17:39:31 -0500

    Do fake login

--- src/layout/App.js
+++ src/layout/App.js
@@ -6,26 +6,56 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
+    Link,
+    Redirect,
+    useHistory,
+    useLocation
 } from "react-router-dom";
 
 const routes = [
     {
         path: "/login",
+        exact: true,
+        secureInnerRoute: true,
         component: Login
     },
     {
-        path: "",
+        path: "*",
         exact: true,
+        authRequired: true,
         component: Main,
         routes: [
             {
-                path: "/home",
+                path: "/",
+                exact: true,
                 component: Home
+            },
+            {
+                path: "/home",
+                exact: true,
+                component: Home
+            },
+            {
+                path: "/*",
+                component: NoMatch
             }
         ]
     }
 ];
+
+const fakeAuth = {
+    isAuthenticated: () => {
+        return localStorage.getItem('isAuthenticated') == 1;
+    },
+    authenticate(cb) {
+        localStorage.setItem('isAuthenticated', 1);
+        setTimeout(cb, 100); // fake async
+    },
+    signout(cb) {
+        localStorage.setItem('isAuthenticated', 0);
+        setTimeout(cb, 100);
+    }
+};
 
 class App extends Component {
 
@@ -33,13 +63,70 @@ class App extends Component {
         return (
             <Router>
                 <Switch>
-                    {routes.map((route, i) => (
-                      <RouteWithSubRoutes key={i} {...route} />
-                    ))}
+                    {routes.map((route, i) => {
+                        if (route.secureInnerRoute) {
+                            return (
+                                <SecureInnerRoute path={route.path} key={i}>
+                                    <RouteWithSubRoutes key={i} {...route} />
+                                </SecureInnerRoute>
+                            );
+                        }
+                        else if (route.authRequired) {
+                            return (
+                                <PrivateRoute path={route.path} key={i}>
+                                    <RouteWithSubRoutes key={i} {...route} />
+                                </PrivateRoute>
+                            );
+                        }
+                        else {
+                            return (
+                                <RouteWithSubRoutes key={i} {...route} />
+                            );
+                        }
+                    })}
                 </Switch>
             </Router>
         );
     }
+}
+
+function SecureInnerRoute({ children, ...rest }) {
+    return (
+        <Route
+            {...rest}
+            render={({ location }) =>
+                fakeAuth.isAuthenticated() ? (
+                  <Redirect
+                    to={{
+                      pathname: "/home"
+                    }}
+                  />
+                ) : (
+                  children
+                )
+            }
+        />
+    );
+}
+
+function PrivateRoute({ children, ...rest }) {
+    return (
+        <Route
+            {...rest}
+            render={({ location }) =>
+                fakeAuth.isAuthenticated() ? (
+                  children
+                ) : (
+                  <Redirect
+                    to={{
+                      pathname: "/login",
+                      state: { from: location }
+                    }}
+                  />
+                )
+            }
+        />
+    );
 }
 
 function RouteWithSubRoutes(route) {
@@ -55,7 +142,22 @@ function RouteWithSubRoutes(route) {
 }
 
 function Login() {
-  return <h2>Login</h2>;
+  let history = useHistory();
+  let location = useLocation();
+
+  let { from } = location.state || { from: { pathname: "/" } };
+  let login = () => {
+    fakeAuth.authenticate(() => {
+      history.replace(from);
+    });
+  };
+
+  return (
+    <div>
+      <p>You must log in to view the page at {from.pathname}</p>
+      <button onClick={login}>Log in</button>
+    </div>
+  );
 }
 
 function Home() {
@@ -73,6 +175,18 @@ function Home() {
             </section>
         </>
     );
+}
+
+function NoMatch() {
+  let location = useLocation();
+
+  return (
+    <div>
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
+    </div>
+  );
 }
 
 export default App;
