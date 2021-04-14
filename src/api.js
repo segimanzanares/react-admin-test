813066c2447c6828275f4ca5fcfe7016ad298a86
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2021-04-14 16:02:05 -0500

    Improve handling api errors

--- src/api.js
+++ src/api.js
@@ -24,40 +24,42 @@ export default function (method, url, data, config) {
     return axios[method](apiBaseUrl + url, data, options)
         .then(response => response)
         .catch(error => {
-            if (error.response.status === 422) {
-                for (var key in error.response.data.errors) {
-                    if (error.response.data.errors.hasOwnProperty(key)) {
-                        //vm.$set(vm.errors, key, error.response.data.errors[key]);
-                    }
-                }
-                //toastr['error']("Algunos datos son incorrectos.");
+            let parsedError = {
+                status: error.response.status,
+                code: error.response.data?.code,
+                errors: error.response.data?.errors,
+                message: error.response.data?.message,
             }
-            else {
-                //toastr['error'](error.response.data.message);
+            if (parsedError.status === 401) {
+                /*this.storage.removeItem('app-token');
+                this.router.navigate(['login']);*/
             }
-            /*if (error.status === 401 && this && this.router) {
-                this.storage.removeItem('app-token');
-                this.router.navigate(['login']);
-            }
-            else if (error.status === 403) {
-                this.toastr.error(error.error.message, '', {
-                    timeOut: 10000
-                });
-                this.router.navigate(['home']);
-            }
-            else if (error.status === 429) {
-                this.toastr.error("Se excedió el límite de peticiones al API. Vuelva a intentarlo en 1 minuto.", '', {
-                    timeOut: 10000
-                });
-            }
-            else if (error.status === 500) {
-                let msg = "An error occurred on the server when your request was being processed, we will solve the problem as soon as possible.";
-                this.translate.get(msg, {}).subscribe((trans) => {
-                    this.toastr.error(trans, '', {
+            else if (parsedError.status === 403) {
+                if (parsedError.message) {
+                    window.toastr.error(parsedError.message, '', {
                         timeOut: 10000
                     });
+                }
+                //this.router.navigate(['home']);
+            }
+            else if (parsedError.status === 422) {
+                if (parsedError.message) {
+                    window.toastr.error(parsedError.message, '', {
+                        timeOut: 10000
+                    });
+                }
+            }
+            else if (parsedError.status === 429) {
+                window.toastr.error("Se excedió el límite de peticiones al API. Vuelva a intentarlo en 1 minuto.", '', {
+                    timeOut: 10000
                 });
-            }*/
-            return Promise.reject(error);
+            }
+            else if (parsedError.status === 500) {
+                let msg = "An error occurred on the server when your request was being processed, we will solve the problem as soon as possible.";
+                window.toastr.error(msg, '', {
+                    timeOut: 10000
+                });
+            }
+            return Promise.reject(parsedError);
         });
     }
