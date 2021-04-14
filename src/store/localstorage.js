06549ba2e738a8ad63e389a8a6797ddaa4d934b6
Segifredo Manzanares <segifredo.manzanares@copyleft.com.mx>
2021-04-14 16:11:40 -0500

    Encrypt auth data

--- src/store/localstorage.js
+++ src/store/localstorage.js
@@ -6,7 +6,7 @@ export const loadState = () => {
         if (serializedData === null) {
             return undefined
         }
-        let auth = JSON.parse(serializedData)
+        let auth = JSON.parse(atob(serializedData))
         axios.defaults.headers.common['Authorization'] = auth.auth.token_type + ' ' + auth.auth.access_token;
         return { auth }
     } catch (error) {
@@ -17,7 +17,7 @@ export const loadState = () => {
 export const saveState = (state) => {
     try {
         let serializedData = JSON.stringify(state.auth)
-        localStorage.setItem('session', serializedData)
+        localStorage.setItem('session', btoa(serializedData))
     } catch (error) {
         console.log(error)
     }
